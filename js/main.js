367d6c62608dce098f03da13caad70a345a2888e
Yusuf <thedriftmaniaka1997@gmail.com>
2021-04-15 10:24:03 +0300

    third

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log("test")
+let offerBtn = document.getElementById('offerBtn')
+
+offerBtn.addEventListener('click',secondPageFunction)
+
+let firstPageDiv = document.querySelector('.front-page');
+let secondPageDiv = document.querySelector('.secondPage');
+function secondPageFunction(){
+    firstPageDiv.style.display='none';
+    secondPageDiv.style.display = 'block';
+}
+
+let backBtn = document.getElementById("backBtn");
+
+backBtn.addEventListener('click',firstPageFunction)
+
+function firstPageFunction(){
+    secondPageDiv.style.display = 'none';
+    firstPageDiv.style.display='block';
+}
+
+
+let thirdPageDiv = document.querySelector('.thirdPage')
+
+thirdPageDiv.addEventListener('click',checkDivision)
+
+function checkDivision(e){
+    let text = e.target.src
+    text = text.slice(40,text.length-4).replace("_"," ");
+    console.log(text)
+    document.getElementById('temp').innerHTML = text;
+}
+
